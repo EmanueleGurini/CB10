17ae9e84deb72109d4417f846ec24d0cc2a17fe7
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-03 13:03:37 +0200

    feat: add input handler

--- 03-07-24/react-form/src/App.jsx
+++ 03-07-24/react-form/src/App.jsx
@@ -1,9 +1,34 @@
 //import styles from "./app.module.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState({
+    name: "",
+    surname: "",
+    city: "",
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setInput((prevState) => ({ prevState, [name]: value }));
+  };
   return (
     <>
-      <h1>hello form</h1>
+      <form>
+        <label>Nome</label>
+        <input name="name" onChange={(e) => handleChange(e)} />
+
+        <label>Cognome</label>
+        <input name="surname" onChange={(e) => handleChange(e)} />
+
+        <label>Città</label>
+        <input name="city" onChange={(e) => handleChange(e)} />
+      </form>
+      <h1>{input.name}</h1>
+      <h1>{input.surname}</h1>
+      <h1>{input.city}</h1>
     </>
   );
 }
