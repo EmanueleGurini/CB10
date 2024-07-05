c6b8797f9d6d20eb61dd31427d187e43e1fca3cc
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-05 11:34:09 +0200

    feat: add input status

--- 05-07-24/react-mortage-calculator/src/App.jsx
+++ 05-07-24/react-mortage-calculator/src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [input, setInput] = useState({
+    mortageAmount: 0,
+    mortageTerm: 0,
+    interestRate: 0,
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("ciao sono io");
+  };
+
+  const handleChange = (e) => {
+    setInput((prevState) => {
+      //const name = e.target.name
+      //const value = e.target.value
+
+      // Destructuring perché e.target è un oggetto!!!
+      const { name, value } = e.target;
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
   };
 
   return (
@@ -11,23 +32,47 @@ function App() {
       {/* Start SX */}
       <div className={styles.container__sx}>
         <h1>Mortage Calculator</h1>
+
+        {/* Start FORM */}
         <form onSubmit={handleSubmit}>
           <label>Mortage Amount</label>
-          <input type="text" />
+          <input
+            name="mortageAmount"
+            type="number"
+            inputMode="numeric"
+            placeholder="Mortage Amount"
+            onChange={handleChange}
+          />
 
           <label>Mortage Term</label>
-          <input type="text" />
+          <input
+            name="mortageTerm"
+            type="number"
+            inputMode="numeric"
+            placeholder="Mortage Term"
+            onChange={handleChange}
+          />
 
           <label>Iterest Rate</label>
-          <input type="text" />
+          <input
+            name="interestRate"
+            type="number"
+            inputMode="numeric"
+            placeholder="Interest Rate"
+            onChange={handleChange}
+          />
+
           <input type="submit" />
         </form>
+        {/* End FORM */}
       </div>
       {/* End SX */}
       {/* Start DX */}
       <div className={styles.container__dx}>
         <h2>Your Result</h2>
-        <h3>Risultato -- da sostituire</h3>
+        <h3>Mortange Amount:{input.mortageAmount}</h3>
+        <h3>Mortange Term:{input.mortageTerm}</h3>
+        <h3>Interest Rate:{input.interestRate}</h3>
       </div>
 
       {/* End DX */}
