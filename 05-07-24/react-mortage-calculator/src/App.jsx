21f7e314025f6f60d7f5d165c21a06270ebb2b1b
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-05 10:37:22 +0200

    feat: add mortage calculator component and style

--- 05-07-24/react-mortage-calculator/src/App.jsx
+++ 05-07-24/react-mortage-calculator/src/App.jsx
@@ -1,10 +1,37 @@
-//import styles from "./App.module.css";
+import styles from "./App.module.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ciao sono io");
+  };
+
   return (
-    <>
-      <h1>Mortage Calculator</h1>
-    </>
+    <div className={styles.container}>
+      {/* Start SX */}
+      <div className={styles.container__sx}>
+        <h1>Mortage Calculator</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Mortage Amount</label>
+          <input type="text" />
+
+          <label>Mortage Term</label>
+          <input type="text" />
+
+          <label>Iterest Rate</label>
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      </div>
+      {/* End SX */}
+      {/* Start DX */}
+      <div className={styles.container__dx}>
+        <h2>Your Result</h2>
+        <h3>Risultato -- da sostituire</h3>
+      </div>
+
+      {/* End DX */}
+    </div>
   );
 }
 
