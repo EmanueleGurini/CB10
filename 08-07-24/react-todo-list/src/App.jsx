009905ef83f815a8d9d5e92dba16825e50992d62
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-08 12:37:20 +0200

    feat: add todo functions

--- 08-07-24/react-todo-list/src/App.jsx
+++ 08-07-24/react-todo-list/src/App.jsx
@@ -1,10 +1,60 @@
-import "./App.module.css";
+import styles from "./App.module.css";
+import { useState } from "react";
+
+const initialTodos = [
+  {
+    id: self.crypto.randomUUID(),
+    title: "Fare la spesa",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    title: "Rinnovare assicurazione",
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (!input.length) return;
+
+    const id = self.crypto.randomUUID();
+    const title = input;
+    setTodos([...todos, { id, title }]);
+    setInput("");
+    console.log("dopo:", todos);
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleDelete = (e) => {
+    const newList = todos.filter((todo) => todo.id !== e.target.id);
+    setTodos(newList);
+  };
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className={styles.container}>
+      <input value={input} placeholder="ToDo" onChange={handleChange} />
+      <button onClick={addTodo}>Add</button>
+      <div className={styles.container__list}>
+        <h2>List di Todo</h2>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                {todo.title}{" "}
+                <button id={todo.id} onClick={handleDelete}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 }
 
