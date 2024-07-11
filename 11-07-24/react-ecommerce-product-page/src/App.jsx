bedfdfc59c72c23f8c4a2d5cbc19302428774eab
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-11 11:20:13 +0200

    feat: add counter component without logic

--- 11-07-24/react-ecommerce-product-page/src/App.jsx
+++ 11-07-24/react-ecommerce-product-page/src/App.jsx
@@ -1,14 +1,16 @@
 //import styles from "./App.module.css";
 import Button from "./components/button/Button";
+import Counter from "./components/counter/Counter";
 import IconCart from "./components/icons/IconCart";
 
 function App() {
   return (
     <>
       <h1>hello, world</h1>
-      <Button text={"Add to cart"}>
+      <Button>
         <IconCart fill="#000" />
       </Button>
+      <Counter />
     </>
   );
 }
--- 11-07-24/react-ecommerce-product-page/src/components/button/Button.jsx
+++ 11-07-24/react-ecommerce-product-page/src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import styles from "./Button.module.css";
 
-function Button({ children, text = "Add to card" }) {
+function Button({ children, text = "Add to cart" }) {
   return (
     <button className={classNames(styles.button)}>
       {children}
