04b142d39abdce830ca0a9e2703d4c819ba9f8f1
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-11 11:05:09 +0200

    feat: :sparkles: add generic button

--- 11-07-24/react-ecommerce-product-page/src/App.jsx
+++ 11-07-24/react-ecommerce-product-page/src/App.jsx
@@ -1,9 +1,14 @@
 //import styles from "./App.module.css";
+import Button from "./components/button/Button";
+import IconCart from "./components/icons/IconCart";
 
 function App() {
   return (
     <>
       <h1>hello, world</h1>
+      <Button text={"Add to cart"}>
+        <IconCart fill="#000" />
+      </Button>
     </>
   );
 }
