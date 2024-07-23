72037a84d8d99deca49faefff33a12f9f58dbbbd
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-07-23 12:32:02 +0200

    feat: add detail + errror + loading handling

--- 22-07-24/react-leggibook/src/main.jsx
+++ 22-07-24/react-leggibook/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import DefaultLayout from "./layouts/DefaultLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BookDetailPage from "./pages/BookDetailPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "books/:id",
+        element: <BookDetailPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
